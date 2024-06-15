21fb684591937d123ab3fb52c1715c97c1ccf7e9
MathiasQM <77293663+MathiasQM@users.noreply.github.com>
2024-06-15 21:48:00 +0200

    Update firebaseAdminConfig.js

--- server/firebaseAdminConfig.js
+++ server/firebaseAdminConfig.js
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 
-import serviceAccount from "@/service-account.json" assert { type: "json" };
+import serviceAccount from "./service-account.json" assert { type: "json" };
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
