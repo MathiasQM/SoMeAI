bcabc6938b945bdd72dcc1de79aec7f23417501a
MathiasQM <77293663+MathiasQM@users.noreply.github.com>
2024-06-17 15:04:53 +0200

    Update firebaseAdminConfig.js

--- server/firebaseAdminConfig.js
+++ server/firebaseAdminConfig.js
@@ -1,12 +1,11 @@
 import admin from "firebase-admin";
-import { useRuntimeConfig } from "@nuxtjs/composition-api";
 
 const config = useRuntimeConfig();
 
 const serviceAccount = {
   projectId: config.private.serviceAccount.projectId,
   clientEmail: config.private.serviceAccount.clientEmail,
-  privateKey: config.private.serviceAccount.privateKey,
+  privateKey: config.private.serviceAccount.privateKey.replace(/\\n/g, "\n"),
 };
 
 // Initialize the Firebase Admin SDK using the service account
