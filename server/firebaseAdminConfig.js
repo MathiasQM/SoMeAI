aa1f1682398493b127c784e8f7dc2b0441d63877
MathiasQM <77293663+MathiasQM@users.noreply.github.com>
2024-06-18 09:55:20 +0200

    test

--- server/firebaseAdminConfig.js
+++ server/firebaseAdminConfig.js
@@ -1,11 +1,19 @@
 import admin from "firebase-admin";
-import fs from "fs";
-
-// Assuming GOOGLE_APPLICATION_CREDENTIALS contains the path to the secret file
-const serviceAccountPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
 // Read the service account key from the secret file
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+const serviceAccount = {
+  type: process.env.SERVICE_ACCOUNT_TYPE,
+  project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+  private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+  private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+  client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
+  auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
+  token_uri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.SERVICE_ACCOUNT_UNIVERSE_DOMAIN,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
